b687095c6efc0283d27bd78d2c3e4bdfbf1bb364
Maria <mariae.f.gurgel@gmail.com>
2025-01-29 11:02:31 -0300

    Estudo

---
+++ jogo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UserApp
+{
+    class Program
+    {
+        string palavrachave = "cavalo";
+    bool continuar = true;
+    public void ExecutarJogo()
+     {
+        while (continuar)
+        {
+            Console.WriteLine("Bem-vindo(a) ao jogo da forca!");
+            Console.WriteLine("DICA: A palavra secreta é um animal mamífero");
+            Console.WriteLine("Digite uma letra");
+            string letra = Console.ReadLine();
+
+            int tentativas = 4;
+
+            for (int i=0; i < tentativas; i++) 
+            {
+                string[] letrasCertas = { "-", "-", "-", "-", "-", "-" };
+                  
+                if (palavrachave.Contains(letra))
+                {
+                    Console.WriteLine("Parabéns! A letra está na palavra secreta");
+
+                }
+                else
+                {
+                    Console.WriteLine("Errou! Tente novamente");
+                }
+            }
+                continuar = false;
+        }
+    }  
+    }
+}
