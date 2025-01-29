aeaf60fa71f575486f3aca32752ef49a10cd5a19
Maria <mariae.f.gurgel@gmail.com>
2025-01-29 11:59:28 -0300

    Melhorias

--- jogo/Program.cs
+++ jogo/Program.cs
@@ -1,39 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace UserApp
+class Program
 {
-    class Program
+    static void Main()
     {
-        string palavrachave = "cavalo";
-    bool continuar = true;
+        Program jogo = new Program();
+        jogo.ExecutarJogo();
+    }
+
     public void ExecutarJogo()
-     {
+    {
+        char[] palavraChave = {'c','a','v','a','l','o'};
+        char[] palavraOculta = new string('_', palavraChave.Length).ToCharArray();
+        bool continuar = true;
+        int tentativas = 0;
+
+        Console.WriteLine("Bem-vindo(a) ao jogo da forca!");
+        Console.WriteLine("DICA: A palavra secreta é um animal mamífero");
+
         while (continuar)
         {
-            Console.WriteLine("Bem-vindo(a) ao jogo da forca!");
-            Console.WriteLine("DICA: A palavra secreta é um animal mamífero");
-            Console.WriteLine("Digite uma letra");
-            string letra = Console.ReadLine();
+            Console.WriteLine("Palavra: " + new string(palavraOculta));
+            Console.WriteLine("Digite uma letra:");
+            char letra = Console.ReadLine()?.FirstOrDefault() ?? ' ';;
+            Console.WriteLine("\nPalavra: " + new string(palavraOculta));
 
-            int tentativas = 4;
-
-            for (int i=0; i < tentativas; i++) 
+            if (palavraChave.Contains(letra))
             {
-                string[] letrasCertas = { "-", "-", "-", "-", "-", "-" };
-                  
-                if (palavrachave.Contains(letra))
+                Console.WriteLine("Parabéns! A letra está na palavra secreta");
+                for (int i = 0; i < palavraChave.Length; i++)
                 {
-                    Console.WriteLine("Parabéns! A letra está na palavra secreta");
-
-                }
-                else
-                {
-                    Console.WriteLine("Errou! Tente novamente");
+                    if (palavraChave[i] == letra)
+                    {
+                        palavraOculta[i] = letra;
+                    }
                 }
             }
+            else
+            {
+                Console.WriteLine("Errou! Tente novamente");
+            }
+
+            tentativas++;
+
+            if (tentativas >= 10)
+            {
+                Console.WriteLine("Ops! Parece que acabaram suas tentativas.");
                 continuar = false;
+            }
         }
-    }  
     }
 }
